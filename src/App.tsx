a66a3fec904aa8fd62ec66824df5fa1aa7bcb49a
Aradhya Singh <jp3832779@gmail.com>
2025-04-26 22:25:38 +0530

    added tailwind and shadcn configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="wrapper w-full p-4 min-h-screen bg-slate-200 overflow-x-hidden">
+      <Button>App</Button>
+    </div>
+  );
 };
 
 export default App;
